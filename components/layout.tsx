62f0095e0f9573786347b4279d76003a952694e1
Eric Ziegler <eric.ziegler@protonmail.com>
2024-06-30 10:25:27 -0600

    upgrade deps, add vercel analytics

--- components/layout.tsx
+++ components/layout.tsx
@@ -16,67 +16,70 @@
 
 import Link from 'next/link';
 import cn from 'classnames';
-import { useRouter } from 'next/router';
-import { SkipNavContent } from '@reach/skip-nav';
-import { NAVIGATION } from '@lib/constants';
+import {useRouter} from 'next/router';
+import {SkipNavContent} from '@reach/skip-nav';
+import {NAVIGATION} from '@lib/constants';
 import styles from './layout.module.css';
 // import MobileMenu from './mobile-menu';
 import Footer from './footer';
 import React from 'react';
+import {Analytics} from "@vercel/analytics/react"
 
 type Props = {
-  children: React.ReactNode;
-  className?: string;
-  hideNav?: boolean;
-  layoutStyles?: any;
-  isLive?: boolean;
+    children: React.ReactNode;
+    className?: string;
+    hideNav?: boolean;
+    layoutStyles?: any;
+    isLive?: boolean;
 };
 
 export default function Layout({
-  children,
-  className,
-  hideNav,
-  layoutStyles,
-}: Props) {
-  const router = useRouter();
-  const activeRoute = router.asPath;
-  return (
-    <>
-      <div className={styles.background}>
-        {!hideNav && (
-          <header className={cn(styles.header)}>
-            <div className={styles['header-logos']}>
-              {/*<MobileMenu key={router.asPath} />*/}
-              <Link href="/">
-                {/* eslint-disable-next-line */}
-                <a className={styles.logo}>
-                  <p>Did we level up?</p>
-                </a>
-              </Link>
+                                   children,
+                                   className,
+                                   hideNav,
+                                   layoutStyles,
+                               }: Props) {
+    const router = useRouter();
+    const activeRoute = router.asPath;
+    return (
+        <>
+            <Analytics/>
+            <div className={styles.background}>
+                {!hideNav && (
+                    <header className={cn(styles.header)}>
+                        <div className={styles['header-logos']}>
+                            {/*<MobileMenu key={router.asPath} />*/}
+                            <Link href="/">
+                                {/* eslint-disable-next-line */}
+                                <a className={styles.logo}>
+                                    <p>Did we level up?</p>
+                                </a>
+                            </Link>
+                        </div>
+                        {/*<div className={styles.tabs}>*/}
+                        {/*  {NAVIGATION.map(({ name, route }) => (*/}
+                        {/*    <a*/}
+                        {/*      key={name}*/}
+                        {/*      href={route}*/}
+                        {/*      className={cn(styles.tab, {*/}
+                        {/*        [styles['tab-active']]: activeRoute.startsWith(route)*/}
+                        {/*      })}*/}
+                        {/*    >*/}
+                        {/*      {name}*/}
+                        {/*    </a>*/}
+                        {/*  ))}*/}
+                        {/*</div>*/}
+                    </header>
+                )}
+                <div className={styles.page}>
+                    <main className={styles.main} style={layoutStyles}>
+                        <SkipNavContent/>
+                        <div className={cn(styles.full, className)}>{children}</div>
+                    </main>
+                    {!activeRoute.startsWith('/stage') && <Footer/>}
+                </div>
             </div>
-            {/*<div className={styles.tabs}>*/}
-            {/*  {NAVIGATION.map(({ name, route }) => (*/}
-            {/*    <a*/}
-            {/*      key={name}*/}
-            {/*      href={route}*/}
-            {/*      className={cn(styles.tab, {*/}
-            {/*        [styles['tab-active']]: activeRoute.startsWith(route)*/}
-            {/*      })}*/}
-            {/*    >*/}
-            {/*      {name}*/}
-            {/*    </a>*/}
-            {/*  ))}*/}
-            {/*</div>*/}
-          </header>
-        )}
-        <div className={styles.page}>
-          <main className={styles.main} style={layoutStyles}>
-            <SkipNavContent />
-            <div className={cn(styles.full, className)}>{children}</div>
-          </main>
-          {!activeRoute.startsWith('/stage') && <Footer />}
-        </div>
-      </div>
-    </>
-  );
+        </>
+
+    );
 }
